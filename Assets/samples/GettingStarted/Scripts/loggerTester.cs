6b2c61f1839db1128034877731b45c2364717385
wanxwang <wanxin-wang@uiowa.edu>
2020-03-30 16:56:10 -0500

    logger correctness test with photon

---
+++ Assets/samples/GettingStarted/Scripts/loggerTester.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+public class loggerTester : MonoBehaviour {
+	loggerSrvLib.Logger m_logger;
+	Random m_rnd = new Random();
+	List<string> m_lstStandard = new List<string>();
+	// Use this for initialization
+	void Start () {
+		m_logger = new loggerSrvLib.Logger();
+		m_logger.Create("test_verifying.txt");
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		int n = Random.Range(2, 4096);
+		string item = new string('b', n);
+				item = '[' + item + ']';
+		m_logger.LogOut(item);
+		m_lstStandard.Add(item);
+	}
+
+    void OnDestroy()  {
+    	m_logger.Close();
+    	m_logger = null;
+    	FileStream fs = new FileStream("test_standard.txt"
+										   , FileMode.CreateNew
+										   , FileAccess.Write);
+		BufferedStream buf = new BufferedStream(fs);
+		foreach (string item in m_lstStandard)
+		{
+			byte[] bytes = Encoding.Unicode.GetBytes(item);
+			buf.Write(bytes, 0, bytes.Length);
+		}
+		buf.Flush();
+    }
+}
