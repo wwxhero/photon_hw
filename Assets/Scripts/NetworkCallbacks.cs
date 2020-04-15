57b166d2f0422cfe484f1cd3ffdf52fd965400df
wanxwang <wanxin-wang@uiowa.edu>
2020-04-14 23:59:28 -0500

    visual test for joint prototype

--- Assets/Scripts/NetworkCallbacks.cs
+++ Assets/Scripts/NetworkCallbacks.cs
@@ -4,38 +4,45 @@ using UnityEngine;
 
 namespace Bolt.Samples.GettingStarted
 {
-    [BoltGlobalBehaviour("Tutorial1")]
-    public class NetworkCallbacks : Bolt.GlobalEventListener
-    {
-        List<string> logMessages = new List<string>();
+	[BoltGlobalBehaviour("Tutorial1")]
+	public class NetworkCallbacks : Bolt.GlobalEventListener
+	{
+		List<string> logMessages = new List<string>();
 
-        public override void SceneLoadLocalDone(string scene)
-        {
-            // randomize a position
-            var spawnPosition = new Vector3(Random.Range(-8, 8), 1, Random.Range(-8, 8));
+		public override void SceneLoadLocalDone(string scene)
+		{
+			// randomize a position
+			var spawnPosition = new Vector3(Random.Range(-8, 8), 1, Random.Range(-8, 8));
 
-            // instantiate cube
-            BoltNetwork.Instantiate(BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
-        }
+			// instantiate cube
+			BoltEntity entity_0 = BoltNetwork.Instantiate(BoltPrefabs.Joint, spawnPosition, Quaternion.identity);
+			spawnPosition.x += 1f;
+			BoltEntity entity_1 = BoltNetwork.Instantiate(BoltPrefabs.Joint, spawnPosition, Quaternion.identity);
+			spawnPosition.x += 1f;
+			BoltEntity entity_2 = BoltNetwork.Instantiate(BoltPrefabs.Joint, spawnPosition, Quaternion.identity);
 
-        public override void OnEvent(LogEvent evnt)
-        {
-            logMessages.Insert(0, evnt.Message);
-        }
+            entity_1.SetParent(entity_0);
+            entity_2.SetParent(entity_1);
+		}
 
-        void OnGUI()
-        {
-            // only display max the 5 latest log messages
-            int maxMessages = Mathf.Min(5, logMessages.Count);
+		public override void OnEvent(LogEvent evnt)
+		{
+			logMessages.Insert(0, evnt.Message);
+		}
 
-            GUILayout.BeginArea(new Rect(Screen.width / 2 - 200, Screen.height - 100, 400, 100), GUI.skin.box);
+		void OnGUI()
+		{
+			// only display max the 5 latest log messages
+			int maxMessages = Mathf.Min(5, logMessages.Count);
 
-            for (int i = 0; i < maxMessages; ++i)
-            {
-                GUILayout.Label(logMessages[i]);
-            }
+			GUILayout.BeginArea(new Rect(Screen.width / 2 - 200, Screen.height - 100, 400, 100), GUI.skin.box);
 
-            GUILayout.EndArea();
-        }
-    }
+			for (int i = 0; i < maxMessages; ++i)
+			{
+				GUILayout.Label(logMessages[i]);
+			}
+
+			GUILayout.EndArea();
+		}
+	}
 }
---
+++ Assets/Scripts/tests/TestJoints.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestJoints : Bolt.EntityBehaviour<IJointState> {
+
+	// Use this for initialization
+
+	private List<Transform> m_lstChain = new List<Transform>();
+
+	public override void Attached()
+	{
+		state.SetTransforms(state.JointTransform, transform);
+		if (null == transform.parent)
+		{
+			Queue<Transform> queBFS = new Queue<Transform>();
+			queBFS.Enqueue(transform);
+			while (queBFS.Count > 0)
+			{
+				Transform p = queBFS.Dequeue();
+				m_lstChain.Add(p);
+				foreach (Transform c in p)
+				{
+					queBFS.Enqueue(c);
+				}
+			}
+		}
+	}
+
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!entity.IsAttached
+		 || !entity.IsOwner
+		 || !(m_lstChain.Count > 0))
+			return;
+
+		Transform refTran = null;
+		for (int nKey = (int)KeyCode.Alpha0; nKey < (int)KeyCode.Alpha9 + 1; nKey ++)
+		{
+			if (Input.GetKeyDown((KeyCode)nKey))
+			{
+				refTran = m_lstChain[nKey - (int)KeyCode.Alpha0];
+			}
+		}
+		if (null != refTran)
+		{
+			const float c_angleDelta = 5;
+            refTran.Rotate(new Vector3(0, c_angleDelta, 0));
+		}
+	}
+
+	void OnDestroy()
+	{
+	}
+}
