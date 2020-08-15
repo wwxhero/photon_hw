e07262671d08861e42d62a89c628da28b9537040
wanxwang <wanxin-wang@uiowa.edu>
2020-08-14 21:25:39 -0500

    joint error visualization prototyping is ported

---
+++ Assets/Scripts/ErrorTr.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ErrorTr : MonoBehaviour {
+    public float m_Error = 0f;
+    public void Start()
+    {
+        m_Error = 0.5f;
+    }
+}
